cb774935821760d8f3c5e83028948b7f94316326
Koen Van Daele <koen_van_daele@telenet.be>
2013-10-23 17:30:23 +0200

    Added first WSA elements.

---
+++ crabpy/wsa.py
@@ -0,0 +1,32 @@
+from suds.sudsobject import Object
+from suds.sax.element import Element
+
+import uuid
+
+wsa = ('wsa', 'http://www.w3.org/2005/08/addressing')
+
+class Action(Object):
+    def __init__(self, action):
+        super(Action, self).__init__()
+        self.action = action
+                    
+    def xml(self):
+        action = Element('Action', ns=wsa)
+        action.setText(self.action)
+        return action
+                                                
+class MessageID(Object):
+    def xml(self):
+        messageid = Element('MessageID', ns=wsa)
+        messageid.setText('uuid: ' + str(uuid.uuid4()))
+        return messageid
+                
+class To(Object):
+    def __init__(self, location):
+        super(To, self).__init__()
+        self.location = location
+                        
+    def xml(self):
+        to = Element('To', ns=wsa)
+        to.setText(self.location)
+        return to
