0bf181d76b455f50fdf5aa6e6232efe65d628bc8
Koen Van Daele <koen_van_daele@telenet.be>
2013-10-24 11:57:08 +0200

    Completed unit tests for wsa.

--- crabpy/tests/test_wsa.py
+++ crabpy/tests/test_wsa.py
@@ -18,3 +18,16 @@ class ElementTests(unittest.TestCase):
         act.setText('http://soap.test.org/test')
         self.assertEqual(act, a.xml())
 
+    def testTo(self):
+        from crabpy.wsa import To
+        t = To('http://ws.agiv.be/capakeyws/nodataset.asmx')
+        to = Element('To', ns=wsa)
+        to.setText('http://ws.agiv.be/capakeyws/nodataset.asmx')
+        self.assertEqual(to, t.xml())
+
+    def testMessageId(self):
+        from crabpy.wsa import MessageID
+        mid = MessageID()
+        # Test that we have the correct element. Ignores content.
+        messageid = Element('MessageID', ns=wsa)
+        self.assertEqual(messageid, mid.xml())
