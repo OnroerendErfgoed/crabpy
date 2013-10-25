0453acf54aa4f923aecdd3ac4bed80bfd9ba4e2e
Koen Van Daele <koen_van_daele@telenet.be>
2013-10-25 14:03:14 +0200

    Set version as 0.1.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
 
 setup(
     name='crabpy',
-    version='0.1.0a1',
+    version='0.1.0',
     description='Interact with AGIV webservices.',
     long_description=open('README.rst').read() + '\n\n' +
                      open('CHANGES.rst').read(),
