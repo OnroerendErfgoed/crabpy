72934922bffa35e85b49eefd9da1b51c7975601a
Koen Van Daele <koen_van_daele@telenet.be>
2013-10-25 10:41:48 +0200

    More extensive capakey example.

--- examples/capakey.py
+++ examples/capakey.py
@@ -5,6 +5,18 @@ capakey = capakey_factory(
     password='PASSWORD'
 )
 
+print capakey
+
 res = capakey_request(capakey, 'ListAdmGemeenten', 1)
 
 print res
+
+res = capakey_request(capakey, 'ListKadAfdelingenByNiscode', 44021, 1)
+
+print res
+
+print capakey_request(capakey, 'ListKadSectiesByKadAfdelingcode', 44021)
+
+print capakey_request(capakey, 'ListKadPerceelsnummersByKadSectiecode', 44021, 'A', 1)
+
+print capakey_request(capakey, 'GetKadPerceelsnummerByCaPaKey', '44021A3675/00A000')
