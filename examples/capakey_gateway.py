68e6a0ff7e86c99bb3b7e588c769bf3d8099f3ff
Koen Van Daele <koen_van_daele@telenet.be>
2013-11-27 22:05:19 +0100

    Added example scripts for capakey gateway.
    With and without caching.

--- examples/capakey_gateway.py
+++ examples/capakey_gateway.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+'''
+This script demonstrates using the capakey gateway to walk the entire
+cadastral tree of a `gemeente`.
+'''
+
 from crabpy.client import capakey_factory, capakey_request
 
 capakey = capakey_factory(
@@ -9,34 +15,12 @@ from crabpy.gateway.capakey import CapakeyGateway
 
 g = CapakeyGateway(capakey)
 
-gent = g.get_gemeente_by_id(44021)
+gemeente = g.get_gemeente_by_id(45062)
 
-print 'Afdelingen in Gent'
-print '------------------'
-
-print [str(a) for a in g.list_kadastrale_afdelingen_by_gemeente(gent)]
-
-print 'Secties in GENT AFD 1'
-print '---------------------'
-
-print [str(s) for s in g.list_secties_by_afdeling(44021)]
-
-print 'Percelen in GENT AFD 1, Sectie A'
-print '--------------------------------'
-
-print [str(p) for p in g.list_percelen_by_sectie(s)]
-
-print 'Perceel 44021A3675/00A000'
-print '-------------------------'
-
-p = g.get_perceel_by_capakey('44021A3675/00A000')
-
-print 'perceel: %s' % p.id
-print 'capakey: %s' % p.capakey
-print 'percid: %s' % p.percid
-print 'grondnummer: %s' % p.grondnummer
-print 'bisnummer: %s' % p.bisnummer
-print 'exponent: %s' % p.exponent
-print 'macht: %s' % p.macht
-print 'sectie: %s' % p.sectie
-print 'afdeling: %s'% p.sectie.afdeling
+print str(gemeente)
+for a in gemeente.afdelingen:
+    print "* %s" % a
+    for s in a.secties:
+        print "\t* %s" % s
+        for p in s.percelen:
+            print "\t\t* %s" % p
---
+++ examples/capakey_gateway_caching.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+'''
+This script demonstrates querying the capakey gateway while maintaining a cache.
+'''
+
+import os
+
+from crabpy.client import capakey_factory, capakey_request
+
+capakey = capakey_factory(
+    user = 'USER',
+    password = 'PASSWORD'
+)
+
+from crabpy.gateway.capakey import CapakeyGateway
+
+root = "./dogpile_data/"
+
+if not os.path.exists(root):
+    os.makedirs(root)
+
+g = CapakeyGateway(
+    capakey,
+    cache_config = {
+        'permanent.backend': 'dogpile.cache.dbm',
+        'permanent.expiration_time': 604800,
+        'permanent.arguments.filename': os.path.join(root, 'capakey_permanent.dbm'),
+        'long.backend': 'dogpile.cache.dbm',
+        'long.expiration_time': 86400,
+        'long.arguments.filename': os.path.join(root, 'capakey_long.dbm'),
+        'short.backend': 'dogpile.cache.dbm',
+        'short.expiration_time': 3600,
+        'short.arguments.filename': os.path.join(root, 'capakey_short.dbm')
+    }
+)
+
+gent = g.get_gemeente_by_id(44021)
+
+print 'Afdelingen in Gent'
+print '------------------'
+
+print [str(a) for a in g.list_kadastrale_afdelingen_by_gemeente(gent)]
+
+print 'Secties in GENT AFD 1'
+print '---------------------'
+
+print [str(s) for s in g.list_secties_by_afdeling(44021)]
+
+print 'Percelen in GENT AFD 1, Sectie A'
+print '--------------------------------'
+
+print [str(p) for p in g.list_percelen_by_sectie(s)]
+
+print 'Perceel 44021A3675/00A000'
+print '-------------------------'
+
+p = g.get_perceel_by_capakey('44021A3675/00A000')
+
+print 'perceel: %s' % p.id
+print 'capakey: %s' % p.capakey
+print 'percid: %s' % p.percid
+print 'grondnummer: %s' % p.grondnummer
+print 'bisnummer: %s' % p.bisnummer
+print 'exponent: %s' % p.exponent
+print 'macht: %s' % p.macht
+print 'sectie: %s' % p.sectie
+print 'afdeling: %s'% p.sectie.afdeling
