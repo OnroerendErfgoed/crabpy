07005826d2e9d4390e1a6029f44bc643ce71a553
Koen Van Daele <koen_van_daele@telenet.be>
2014-09-03 17:27:49 +0200

    Update examples.
    Added an example of walking the tree of administrative units in Belgium.

---
+++ examples/crab_gateway_admin_units.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+This script demonstrates using the crab gateway to walk the entire
+address tree (street and number) of a `gemeente`.
+'''
+
+from crabpy.client import crab_request, crab_factory
+from crabpy.gateway.crab import CrabGateway
+
+g = CrabGateway(crab_factory())
+
+gewesten = g.list_gewesten()
+
+print('Administratieve eenheden in België')
+print('----------------------------------')
+for gw in gewesten:
+    print('\n')
+    print('\t%s' % str(gw))
+    print('\t' + ('-' * len(str(gw))))
+    for p in gw.provincies:
+        print("\n")
+        print('\t\t%s' % str(p))
+        print('\t\t' + ('-' * len(str(p))))
+        for g in p.gemeenten:
+            print('\t\t\t* %s (%s)' % (g.naam, g.niscode))
