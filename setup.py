1cf8eac0a404132022b103e8965d00ed26e089da
Koen Van Daele <koen_van_daele@telenet.be>
2014-09-05 19:56:06 +0200

    Bump versions.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = [
 
 setup(
     name='crabpy',
-    version='0.4.0',
+    version='0.4.1',
     description='Interact with AGIV webservices.',
     long_description=open('README.rst').read() + '\n\n' +
                      open('CHANGES.rst').read(),
