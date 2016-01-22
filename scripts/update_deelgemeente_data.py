40ba353e71eb32cfac99bd5dcf4a154fa2e3f155
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2016-01-22 10:48:05 +0100

    Add deelgemeenten data. Refs #63

---
+++ scripts/update_deelgemeente_data.py
@@ -0,0 +1,28 @@
+import requests
+import tarfile
+import os
+
+from shutil import copyfile, rmtree
+
+version = '0.1.0'
+
+download_url = 'https://github.com/OnroerendErfgoed/deelgemeenten/archive/v%s.tar.gz' % version
+
+localfile = 'v%s.tar.gz' % version
+
+r = requests.get(download_url, stream=True)
+
+with open(localfile, 'wb') as f:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            f.write(chunk)
+
+tar = tarfile.open(localfile)
+tar.extractall()
+tar.close()
+
+datafile = 'deelgemeenten-%s/data/json/deelgemeenten.json' % version
+
+copyfile(datafile, '../crabpy/data/deelgemeenten.json')
+os.remove(localfile)
+rmtree('deelgemeenten-%s' % version)
