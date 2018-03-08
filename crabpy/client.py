8385be66a5aaca5754968d648cfec3c00a34e9ae
Koen Van Daele <koen.vandaele@vlaanderen.be>
2018-03-08 22:59:26 +0100

    Remove capakey. Refs #92.

--- crabpy/client.py
+++ crabpy/client.py
@@ -12,11 +12,6 @@ log = logging.getLogger(__name__)
 
 from suds.client import Client
 
-from suds.wsse import Security
-from crabpy.wsse import UsernameDigestToken
-
-from crabpy.wsa import Action, MessageID, To
-
 
 def crab_factory(**kwargs):
     '''
@@ -43,46 +38,6 @@ def crab_factory(**kwargs):
     return c
 
 
-def capakey_factory(**kwargs):
-    '''
-    Factory that generates a CAPAKEY client.
-
-    A few parameters will be handled by the factory, other parameters will
-    be passed on to the client.
-
-    :param user: `Required.` Username for authenticating with the CAPAKEY
-        service.
-    :param password: `Required.` Password for authenticating with the CAPAKEY
-        service.
-    :param wsdl: `Optional.` Allows overriding the default CAPAKEY wsdl url.
-    :param proxy: `Optional.` A dictionary of proxy information that is passed
-        to the underlying :class:`suds.client.Client`
-    :rtype: :class:`suds.client.Client`
-    '''
-    if 'wsdl' in kwargs:
-        wsdl = kwargs['wsdl']
-        del kwargs['wsdl']
-    else:
-        wsdl = "http://ws.agiv.be/capakeyws/nodataset.asmx?WSDL"
-    if 'user' in kwargs and 'password' in kwargs:
-        user = kwargs['user']
-        password = kwargs['password']
-        del kwargs['user']
-        del kwargs['password']
-    else:
-        raise ValueError(
-            "You must specify a 'user' and a 'password'."
-        )
-    log.info('Creating CAPAKEY client with wsdl: %s', wsdl)
-    c = Client(
-        wsdl,
-        **kwargs
-    )
-    c.capakey_user = user
-    c.capakey_password = password
-    return c
-
-
 def crab_request(client, action, *args):
     '''
     Utility function that helps making requests to the CRAB service.
@@ -94,27 +49,4 @@ def crab_request(client, action, *args):
     .. versionadded:: 0.3.0
     '''
     log.debug('Calling %s on CRAB service.', action)
-    return getattr(client.service, action)(*args)
-
-
-def capakey_request(client, action, *args):
-    '''
-    Utility function that helps making requests to the CAPAKEY service.
-
-    :param client: A :class:`suds.client.Client` for the CAPAKEY service.
-    :param string action: Which method to call, eg. `ListAdmGemeenten`.
-    :returns: Result of the SOAP call.
-    '''
-    security = Security()
-    token = UsernameDigestToken(client.capakey_user, client.capakey_password)
-    security.tokens.append(token)
-    client.set_options(wsse=security)
-
-    cm = getattr(client.service, action)
-    a = Action(cm.method.soap.action)
-    mid = MessageID()
-    t = To(cm.method.location)
-    client.set_options(soapheaders=[a.xml(), t.xml(), mid.xml()])
-
-    log.debug('Calling %s on CAPAKEY service.', action)
     return getattr(client.service, action)(*args)
