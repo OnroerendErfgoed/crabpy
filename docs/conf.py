9a0d344ec2671bba7dc437b027697f7f9d0df42c
Koen Van Daele <koen_van_daele@telenet.be>
2025-01-17 23:20:29 +0100

    Update intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -332,4 +332,4 @@ epub_copyright = "2013-2017, Flanders Heritage Agency"
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"http://docs.python.org/": None}
+intersphinx_mapping = {'python': ("https://docs.python.org/3", None)}
